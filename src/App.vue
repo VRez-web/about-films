45e53d8d83eda157b7235ae1b897598a2788bd44
VRez-web <vit.79@bk.ru>
2021-02-25 22:04:06 +0300

    added animation for route

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-<AppHeader />
-   <router-view></router-view>
+  <AppHeader />
+  <router-view v-slot="{ Component }">
+    <transition name="scale">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader'
+import AppHeader from "./components/AppHeader";
 export default {
-  components:{AppHeader}
-}
+  components: { AppHeader },
+};
 </script>
 <style lang="scss">
+.scale-enter-active,
+.scale-leave-active {
+  transition: all .5s 0.5s ease;
+}
 
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
 </style>
