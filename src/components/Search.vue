290705711337a5020d49dffff20a7765d19b563a
VRez-web <vit.79@bk.ru>
2021-02-25 22:03:50 +0300

    created new component

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="search">
+    <div class="container">привет</div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: { searchShow: Boolean },
+  data() {
+    return {};
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+.search {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 2;
+}
+</style>
