9a0b8e66978502c45ddc3d44fe72eb03bc1623ad
VRez-web <vit.79@bk.ru>
2021-03-07 17:48:36 +0300

    trying fixed bug(failed)

--- src/components/search/SearchTotal.vue
+++ src/components/search/SearchTotal.vue
@@ -4,33 +4,50 @@
       <div class="search__total-inner">
         <card :data="totalMovies.results" />
       </div>
+      <pagination
+        :data="totalMovies"
+        :totalPages="totalMovies.total_pages"
+        :currentPage="currentPage"
+        @pageChange="pageChange"
+      />
     </div>
   </section>
 </template>
 
 <script>
+import pagination from "../Pagination";
 import card from "../Card";
 import { mapActions } from "vuex";
 export default {
-  components: { card },
+  components: { card, pagination },
   data() {
     return {
       totalMovies: [],
       totalTvShows: [],
       totalPerson: [],
-      query:this.$attrs.query,
-      category:this.$attrs.title
+      query: this.$attrs.query,
+      category: this.$attrs.title,
+      currentPage: this.$store.page,
     };
   },
   methods: {
     ...mapActions(["GET_SEARCH_TOTAL_MOVIES"]),
-  },
-  computed: {
-  },
-  mounted() {
-    if (this.category == "movie") {
-      this.GET_SEARCH_TOTAL_MOVIES(this.query).then((res) => {
+    pageChange(page) {
+      // this.currentPage = page;
+      this.GET_SEARCH_TOTAL_MOVIES(this.query, page).then((res) => {
         this.totalMovies = res;
+        console.log(this.query);
+        console.log(page);
+        console.log(res);
+      });
+    },
+  },
+  computed: {},
+  created() {
+    if (this.category == "movie") {
+      this.GET_SEARCH_TOTAL_MOVIES(this.query, this.currentPage).then((res) => {
+        this.totalMovies = res;
+        this.currentPage = res.page;
       });
     } else if (this.category == "tv-shows") {
     } else {
@@ -41,9 +58,8 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../assets/scss/_vars.scss";
-.search__total{
-
-  &-inner{
+.search__total {
+  &-inner {
     display: flex;
     flex-wrap: wrap;
   }
