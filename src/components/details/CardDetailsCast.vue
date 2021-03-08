313a3d41203b945ff7c89ec1f735cb442741a353
VRez-web <vit.79@bk.ru>
2021-03-08 15:56:27 +0300

    created new component for people in total cast

---
+++ src/components/details/CardDetailsCast.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="cast">
+    <div class="container">
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+data(){},
+methods:{},
+computed:{},
+
+mounted(){
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
