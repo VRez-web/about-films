47b2a060cfae79c9b7b3a71faa5f0a38c36b7e23
VRez-web <vit.79@bk.ru>
2021-03-11 21:26:35 +0300

    now all routes async

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../pages/Home";
-import MoviesTheatres from "../pages/movies/MoviesTheatres";
-import MoviesPopular from "../pages/movies/MoviesPopular";
-import MoviesTopRated from "../pages/movies/MoviesTopRated";
-import MoviesUpcoming from "../pages/movies/MoviesUpcoming";
-import TvShowsToday from "../pages/tvShows/TvShowsToday";
-import TvShowsWeek from "../pages/tvShows/TvShowsWeek";
-// import TvShowsPopular from "../pages/tvShows/TvShowsPopular";
-// import TvShowsTopRated from "../pages/tvShows/TvShowsTopRated";
-// import CardDetails from "../components/details/CardDetails";
-// const CardDetails = () => import("../components/details/CardDetails"),
-//   TvShowsPopular = () => import("../pages/tvShows/TvShowsPopular"),
-//   TvShowsTopRated = () => import("../pages/tvShows/TvShowsTopRated");
-import SearchTotal from "../components/search/SearchTotal";
-import cardDetailsCast from "../components/details/CardDetailsCast";
 const routes = [
   {
     path: "/",
-    component: Home,
+    component:()=> import("../pages/Home"),
   },
   {
     path: "/movies-theatres",
-    component: MoviesTheatres,
+    component:()=> import("../pages/movies/MoviesTheatres")
   },
   {
     path: "/movies-popular",
-    component: MoviesPopular,
+    component:()=> import("../pages/movies/MoviesPopular")
   },
   {
     path: "/movies-topRated",
-    component: MoviesTopRated,
+    component:()=> import("../pages/movies/MoviesTopRated")
   },
   {
     path: "/movies-upcoming",
-    component: MoviesUpcoming,
+    component:()=> import("../pages/movies/MoviesUpcoming"),
   },
   {
     path: "/serials-today",
-    component: TvShowsToday,
+    component:()=> import("../pages/tvShows/TvShowsToday")
   },
   {
     path: "/serials-week",
-    component: TvShowsWeek,
+    component:()=> import("../pages/tvShows/TvShowsWeek"),
   },
   {
     path: "/serials-popular",
@@ -59,13 +44,13 @@ const routes = [
   },
   {
     path: "/:title/total",
-    component: SearchTotal,
+    component:()=> import("../components/search/SearchTotal"),
     name: "search-total",
     props: true,
   },
   {
     path: "/:title/:id/cast",
-    component: cardDetailsCast,
+    component:()=> import("../components/details/CardDetailsCast"),
     name: "card-details-cast",
     props: true,
   },
