010bc1c1d9f792e18c9350169f9c411794da7050
VRez-web <vit.79@bk.ru>
2021-03-17 21:12:57 +0300

    added for all routes-name

--- src/router/index.js
+++ src/router/index.js
@@ -2,55 +2,63 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    component:()=> import("../pages/Home"),
+    component: () => import("../pages/Home"),
   },
   {
+    name: "movies-theatres",
     path: "/movies-theatres",
-    component:()=> import("../pages/movies/MoviesTheatres")
+    component: () => import("../pages/movies/MoviesTheatres"),
   },
   {
+    name: "movies-popular",
     path: "/movies-popular",
-    component:()=> import("../pages/movies/MoviesPopular")
+    component: () => import("../pages/movies/MoviesPopular"),
   },
   {
+    name: "movies-topRated",
     path: "/movies-topRated",
-    component:()=> import("../pages/movies/MoviesTopRated")
+    component: () => import("../pages/movies/MoviesTopRated"),
   },
   {
+    name: "movies-upcoming",
     path: "/movies-upcoming",
-    component:()=> import("../pages/movies/MoviesUpcoming"),
+    component: () => import("../pages/movies/MoviesUpcoming"),
   },
   {
+    name: "serials-today",
     path: "/serials-today",
-    component:()=> import("../pages/tvShows/TvShowsToday")
+    component: () => import("../pages/tvShows/TvShowsToday"),
   },
   {
+    name: "serials-week",
     path: "/serials-week",
-    component:()=> import("../pages/tvShows/TvShowsWeek"),
+    component: () => import("../pages/tvShows/TvShowsWeek"),
   },
   {
+    name: "serials-popular",
     path: "/serials-popular",
-    component:()=> import('../pages/tvShows/TvShowsPopular'),
+    component: () => import("../pages/tvShows/TvShowsPopular"),
   },
   {
+    name: "serials-topRated",
     path: "/serials-topRated",
-    component:()=> import("../pages/tvShows/TvShowsTopRated"),
+    component: () => import("../pages/tvShows/TvShowsTopRated"),
   },
   {
     path: "/:title/:id",
     name: "card-details",
-    component: ()=> import("../components/details/CardDetails"),
+    component: () => import("../components/details/CardDetails"),
     props: true,
   },
   {
     path: "/:title/total",
-    component:()=> import("../components/search/SearchTotal"),
+    component: () => import("../components/search/SearchTotal"),
     name: "search-total",
     props: true,
   },
   {
     path: "/:title/:id/cast",
-    component:()=> import("../components/details/CardDetailsCast"),
+    component: () => import("../components/details/CardDetailsCast"),
     name: "card-details-cast",
     props: true,
   },
