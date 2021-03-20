280a2051e6e291f867a925060e3f21d465ece15c
VRez-web <vit.79@bk.ru>
2021-03-20 23:47:26 +0300

    groundwork for the future

--- src/router/index.js
+++ src/router/index.js
@@ -7,42 +7,42 @@ const routes = [
   },
   {
     name: "movies-theatres",
-    path: "/movies-theatres",
+    path: "movies/movies-theatres",
     component: () => import("../pages/movies/MoviesTheatres"),
   },
   {
     name: "movies-popular",
-    path: "/movies-popular",
+    path: "movies/movies-popular",
     component: () => import("../pages/movies/MoviesPopular"),
   },
   {
     name: "movies-topRated",
-    path: "/movies-topRated",
+    path: "movies/movies-topRated",
     component: () => import("../pages/movies/MoviesTopRated"),
   },
   {
     name: "movies-upcoming",
-    path: "/movies-upcoming",
+    path: "movies/movies-upcoming",
     component: () => import("../pages/movies/MoviesUpcoming"),
   },
   {
     name: "serials-today",
-    path: "/serials-today",
+    path: "serials/serials-today",
     component: () => import("../pages/tvShows/TvShowsToday"),
   },
   {
     name: "serials-week",
-    path: "/serials-week",
+    path: "serials/serials-week",
     component: () => import("../pages/tvShows/TvShowsWeek"),
   },
   {
     name: "serials-popular",
-    path: "/serials-popular",
+    path: "serials/serials-popular",
     component: () => import("../pages/tvShows/TvShowsPopular"),
   },
   {
     name: "serials-topRated",
-    path: "/serials-topRated",
+    path: "serials/serials-topRated",
     component: () => import("../pages/tvShows/TvShowsTopRated"),
   },
   {
