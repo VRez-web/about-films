db24b5ac81f914cef3011b6c373146d2d8916faf
VRez-web <vit.79@bk.ru>
2021-03-24 12:59:43 +0300

    added more logic for more flexibility

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -6,9 +6,10 @@
     @swiper="onSwiper"
     @slideChange="onSlideChange"
   >
-    <swiper-slide v-for="item in data" :key="item.id"
-      ><card :data="[item]" :category="category"/>
-    </swiper-slide>
+    <swiper-slide v-for="item in data" :key="item.id">
+      <card :data="[item]" :category="category" v-if="category != 'people'" />
+      <card-of-people v-else :data="item"/>
+    </swiper-slide >
   </swiper>
 </template>
 
@@ -18,12 +19,13 @@ import { Swiper, SwiperSlide } from "swiper/vue";
 import card from "../components/Card";
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
+import CardOfPeople from "./CardOfPeople.vue";
 SwiperCore.use([Navigation]);
 export default {
-  components: { Swiper, SwiperSlide, card },
-  props:{
-      data:Object,
-      category:String
+  components: { Swiper, SwiperSlide, card, CardOfPeople },
+  props: {
+    data: Object,
+    category: String,
   },
   data() {
     return {};
