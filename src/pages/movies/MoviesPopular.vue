4e167f900e9e32088c5969cc7a6bff4077e47aca
VRez-web <vit.79@bk.ru>
2021-04-03 17:55:22 +0300

    mini refactoring

--- src/pages/movies/MoviesPopular.vue
+++ src/pages/movies/MoviesPopular.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <h2 class="movies__popular-title section__title">Популярное кино</h2>
       <div class="movies__popular-inner section__inner">
-        <card :data="moviesPopular.results" :category="'movie'"/>
+        <card :data="moviesPopular.results" :category="'movie'" />
       </div>
 
       <pagination
@@ -31,32 +31,29 @@ export default {
   },
   methods: {
     ...mapActions(["GET_MOVIES_POPULAR", "GET_MOVIE_DATES"]),
-    pageChange(page) {
-      this.GET_MOVIES_POPULAR(page).then((res) => {
-        this.moviesPopular = res;
-      });
+    async pageChange(page) {
+      const MOVIES_POPULAR = await this.GET_MOVIES_POPULAR(page);
+      this.moviesPopular = MOVIES_POPULAR;
+      this.currentPage = page;
     },
   },
 
-  mounted() {
-    this.GET_MOVIES_POPULAR().then((res) => {
-      this.moviesPopular = res;
-      this.moviesPopular.results.forEach((item) => {
-        this.GET_MOVIE_DATES(item.id).then((resolve) => {
-          resolve.results.forEach((date) => {
-            if (date.iso_3166_1 == "RU") {
-              item.release_date = date.release_dates[0].release_date.slice(
-                0,
-                4
-              );
-            }
-          });
+  async mounted() {
+    const MOVIES_POPULAR = await this.GET_MOVIES_POPULAR();
+    this.moviesPopular = MOVIES_POPULAR;
+
+    this.moviesPopular.results.forEach((item) => {
+      this.GET_MOVIE_DATES(item.id).then((resolve) => {
+        resolve.results.forEach((date) => {
+          if (date.iso_3166_1 == "RU") {
+            item.release_date = date.release_dates[0].release_date.slice(0, 4);
+          }
         });
       });
-
-      this.pages = res.total_pages;
-      this.currentPage = res.page;
     });
+
+    this.pages = MOVIES_POPULAR.total_pages;
+    this.currentPage = MOVIES_POPULAR.page;
   },
 };
 </script>
