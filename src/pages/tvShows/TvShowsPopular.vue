5be9e3f575df67a5b52e72c96846e6dcc49a797d
VRez-web <vit.79@bk.ru>
2021-05-01 20:26:09 +0300

    refactoring component

--- src/pages/tvShows/TvShowsPopular.vue
+++ src/pages/tvShows/TvShowsPopular.vue
@@ -3,12 +3,11 @@
     <div class="container">
       <h2 class="section__title">Популярные сериалы</h2>
       <div class="tv__shows__inner section__inner">
-        <card :data="tvShowsPopular.results" :category="'serial'"/>
+        <card :data="serial.results" :category="serial.type" />
       </div>
 
       <pagination
-        :data="tvShowsPopular"
-        :totalPages="tvShowsPopular.total_pages"
+        :totalPages="pages"
         :currentPage="currentPage"
         @pageChange="pageChange"
       />
@@ -18,33 +17,36 @@
 
 <script>
 import { mapActions } from "vuex";
-import pagination from "../../components/Pagination.vue";
-import card from "../../components/Card";
+import pagination from "@/components/Pagination.vue";
+import card from "@/components/Card";
+
 export default {
   components: { card, pagination },
   data() {
     return {
-      tvShowsPopular: [],
-      pages: "",
+      serial: {},
+      pages: null,
       currentPage: this.$store.page,
     };
   },
   methods: {
-    ...mapActions('serialsPopular',["GET_TV_SHOWS_POPULAR"]),
-    pageChange(page) {
+    ...mapActions("serialsPopular", ["GET_TV_SHOWS_POPULAR"]),
+    async pageChange(page) {
+      const SERIAL = await this.GET_TV_SHOWS_POPULAR(page);
+      this.serial = { ...SERIAL, ...SERIAL.data };
       this.currentPage = page;
-      this.GET_TV_SHOWS_POPULAR((page = this.currentPage)).then((res) => {
-        this.tvShowsPopular = res;
-      });
     },
   },
-  computed: {},
-  mounted() {
-    this.GET_TV_SHOWS_POPULAR().then((res) => {
-      this.tvShowsPopular = res;
-      this.pages = res.total_pages;
-      this.currentPage = res.page;
-    });
+  async created() {
+    try {
+      const SERIAL = await this.GET_TV_SHOWS_POPULAR();
+      this.serial = { ...SERIAL, ...SERIAL.data };
+      this.currentPage = SERIAL.data.page;
+      this.pages = SERIAL.data.total_pages;
+    } catch (e) {
+      // FIXME: сделать модалку для вывода ошибки пользователю
+      console.log(e);
+    }
   },
 };
 </script>
