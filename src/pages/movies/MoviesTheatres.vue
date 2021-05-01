3e3a3029eae284754f62ea0c1be9c3587d30f73a
VRez-web <vit.79@bk.ru>
2021-05-01 19:53:53 +0300

    refactoring component

--- src/pages/movies/MoviesTheatres.vue
+++ src/pages/movies/MoviesTheatres.vue
@@ -5,12 +5,11 @@
         Сейчас в кинотеатрах:
       </h2>
       <div class="movies__theatres-inner section__inner">
-        <card :data="moviesTeatres.results" :category="'movie'" />
+        <card :data="movies.results" :category="movies.type" />
       </div>
 
       <pagination
-        :data="moviesTeatres"
-        :totalPages="moviesTeatres.total_pages"
+        :totalPages="pages"
         :currentPage="currentPage"
         @pageChange="pageChange"
       />
@@ -22,43 +21,35 @@
 import { mapActions } from "vuex";
 import pagination from "../../components/Pagination.vue";
 import card from "../../components/Card";
+
 export default {
   components: { card, pagination },
   data() {
     return {
-      moviesTeatres: [],
-      pages: "",
+      movies: {},
+      pages: null,
       currentPage: this.$store.page,
     };
   },
   methods: {
     ...mapActions("moviesTheatres", ["GET_MOVIES_THEATRES"]),
-    ...mapActions("moviesDates", ["GET_MOVIE_DATES"]),
     async pageChange(page) {
-      const MOVIES_THEATRES = await this.GET_MOVIES_THEATRES(page);
-      this.moviesTeatres = MOVIES_THEATRES;
+      const MOVIES = await this.GET_MOVIES_THEATRES(page);
+      this.movies = { ...MOVIES, ...MOVIES.data };
       this.currentPage = page;
     },
   },
 
-  mounted() {
-    this.GET_MOVIES_THEATRES().then((res) => {
-      this.moviesTeatres = res;
-      this.moviesTeatres.results.forEach((item) => {
-        this.GET_MOVIE_DATES(item.id).then((resolve) => {
-          resolve.results.forEach((date) => {
-            if (date.iso_3166_1 == "RU") {
-              item.release_date = date.release_dates[0].release_date.slice(
-                0,
-                4
-              );
-            }
-          });
-        });
-      });
-      this.pages = res.total_pages;
-      this.currentPage = res.page;
-    });
+  async created() {
+    try {
+      const MOVIES = await this.GET_MOVIES_THEATRES();
+      this.movies = { ...MOVIES, ...MOVIES.data };
+      this.currentPage = MOVIES.data.page;
+      this.pages = MOVIES.data.total_pages;
+    } catch (e) {
+      // FIXME: сделать модалку для вывода ошибки пользователю
+      console.log(e);
+    }
   },
 };
 </script>
