53ed9c77b888ca9aa793af93b0b18f7109352800
VRez-web <vit.79@bk.ru>
2021-05-01 20:26:31 +0300

    moved out of folders

---
+++ src/components/HomeGallery.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="home__gallery">
+    <h2 class="home__gallery-title">
+      <router-link :to="{ name: link }" class="link">{{ title }}</router-link>
+      <i class="icon-right"></i>
+    </h2>
+    <slider :data="dataSlider" id="id" :category="category" />
+  </section>
+</template>
+
+<script>
+import slider from "@/components/Slider";
+
+export default {
+  components: { slider },
+  props: {
+    title: String,
+    link: String,
+    id: String,
+    dataSlider: Object,
+    category: String,
+  },
+};
+</script>
+
+<style>
+</style>
