816d7da30618e64b59d50fa2dcf44fa25191a75a
VRez-web <vit.79@bk.ru>
2021-05-17 22:29:25 +0300

    added new img size from api

--- src/store/index.js
+++ src/store/index.js
@@ -20,14 +20,16 @@ import searchMovies from "./modules/search/searchMovies";
 import searchSerials from "./modules/search/searchSerials";
 import searchPerson from "./modules/search/searchPeople";
 
-import aboutPerson from './modules/person/aboutPerson'
+import aboutPerson from "./modules/person/aboutPerson";
+
 const store = createStore({
   state() {
     return {
       apiKey: process.env.VUE_APP_API_KEY,
       imgUrl: "https://image.tmdb.org/t/p/w342",
+      imgUrlSmall: "https://image.tmdb.org/t/p/w92",
+      imgUrlMedium: "https://image.tmdb.org/t/p/w154",
       imgProfileSize: "http://image.tmdb.org/t/p/w185",
-      page: 1,
     };
   },
   modules: {
