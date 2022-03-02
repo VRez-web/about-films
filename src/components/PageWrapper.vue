7db650ee45e398473b0f36418e7f8740b80b98a9
Victor Reznichenko <vit.79@bk.ru>
2022-03-02 15:15:41 +0300

    feat: create total search page and child component for all pages in future

---
+++ src/components/PageWrapper.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="section__inner">
+    <Card v-for="item in model.results" :model="item" :media-type="checkMediaType(model)"/>
+  </div>
+  <Pagination
+      :current-page="model.page"
+      :total-pages="model.total_pages"
+      @page-change="pageChange"
+  />
+</template>
+
+<script>
+import Pagination from "@/components/Pagination";
+import Card from "@/components/Card";
+import {checkMediaType} from '@/utils/commonFunctions';
+
+export default {
+  components: {
+    Pagination,
+    Card
+  },
+  props: {
+    model: Object
+  },
+  methods: {
+    checkMediaType,
+
+    pageChange(page) {
+      this.$emit('page-change', page)
+    }
+  }
+}
+</script>
